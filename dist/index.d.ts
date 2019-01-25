6de1bd6f620a56f30b3eaf6256f28741965b60ff
Alexandre Piel <alexandre.piel@gmail.com>
2019-01-25 23:02:24 +0100

    update type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,10 +2,11 @@ import { ShepherdConfig } from './shepherd.factory';
 import { Logger } from './logger';
 import { ZigbeeService } from './zigbee.service';
 import { DeviceService } from './device.service';
-export declare function init(config: ShepherdConfig, logger?: Logger): {
-    device: DeviceService;
+export interface ZigbeeAndDevice {
     zigbee: ZigbeeService;
-};
+    device: DeviceService;
+}
+export declare function init(config: ShepherdConfig, logger?: Logger): ZigbeeAndDevice;
 export { useFactory, Shepherd, ShepherdConfig } from './shepherd.factory';
 export { ZigbeeService, eventType, messageType } from './zigbee.service';
 export { DeviceService, Action, Device, DeviceEndPoint, DeviceModel, MappedModel } from './device.service';
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ import { Logger } from './logger';
 import { ZigbeeService } from './zigbee.service';
 import { DeviceService } from './device.service';
 
+export interface ZigbeeAndDevice {
+    zigbee: ZigbeeService;
+    device: DeviceService;
+}
+
 export function init(
     config: ShepherdConfig,
     logger: Logger = new Logger(),
-) {
+): ZigbeeAndDevice {
     const shepherd = useFactory(config, logger);
     const deviceService = new DeviceService(shepherd, logger);
     const zigbeeService = new ZigbeeService(shepherd, deviceService, logger);
